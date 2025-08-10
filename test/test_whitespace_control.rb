0b820a61d831cca879ac1d5e147c4e4fb3dacd0c
James Prior <jamesgr.prior@gmail.com>
2025-08-10 16:45:59 +0100

    Add automatic whitespace trimming feature

--- test/test_whitespace_control.rb
+++ test/test_whitespace_control.rb
@@ -3,6 +3,27 @@
 require "test_helper"
 
 class TestWhitespaceControl < Minitest::Test
+  def test_no_whitespace_control
+    source = <<~LIQUID
+      <ul>
+      {% for x in (1..4) %}
+        <li>{{ x }}</li>
+      {% endfor %}
+      </ul>
+    LIQUID
+
+    expect = <<~LIQUID
+      <ul>\n
+        <li>1</li>\n
+        <li>2</li>\n
+        <li>3</li>\n
+        <li>4</li>\n
+      </ul>
+    LIQUID
+
+    assert_equal(expect, Liquid2.render(source))
+  end
+
   def test_tilde
     source = <<~LIQUID
       <ul>
@@ -22,5 +43,97 @@ class TestWhitespaceControl < Minitest::Test
     LIQUID
 
     assert_equal(expect, Liquid2.render(source))
+  end
+
+  def test_auto_trim_tilde
+    source = <<~LIQUID
+      <ul>
+      {% for x in (1..4) %}
+        <li>{{ x }}</li>
+      {% endfor %}
+      </ul>
+    LIQUID
+
+    expect = <<~LIQUID
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+      </ul>
+    LIQUID
+
+    env = Liquid2::Environment.new(auto_trim: "~")
+
+    assert_equal(expect, env.render(source))
+  end
+
+  def test_auto_trim_hyphen
+    source = <<~LIQUID
+      <ul>
+      {% for x in (1..4) %}
+        <li>{{ x }}</li>
+      {% endfor %}
+      </ul>
+    LIQUID
+
+    expect = <<~LIQUID
+      <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      </ul>
+    LIQUID
+
+    env = Liquid2::Environment.new(auto_trim: "-")
+
+    assert_equal(expect, env.render(source))
+  end
+
+  def test_override_auto_trim
+    source = <<~LIQUID
+      <ul>
+      {% for x in (1..4) ~%}
+        <li>{{ x }}</li>
+      {% endfor %}
+      </ul>
+    LIQUID
+
+    expect = <<~LIQUID
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+      </ul>
+    LIQUID
+
+    env = Liquid2::Environment.new(auto_trim: "-")
+
+    assert_equal(expect, env.render(source))
+  end
+
+  def test_override_auto_trim_with_plus
+    source = <<~LIQUID
+      <ul>
+      {% for x in (1..4) +%}
+        <li>{{ x }}</li>
+      {% endfor +%}
+      </ul>
+    LIQUID
+
+    expect = <<~LIQUID
+      <ul>\n
+        <li>1</li>\n
+        <li>2</li>\n
+        <li>3</li>\n
+        <li>4</li>\n
+      </ul>
+    LIQUID
+
+    env = Liquid2::Environment.new(auto_trim: "-")
+
+    assert_equal(expect, env.render(source))
   end
 end
